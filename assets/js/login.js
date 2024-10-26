815105e148c83879d9e7ea2cc87111576c442470
Chanchal <167846072+Chanchalbundela@users.noreply.github.com>
2024-10-26 20:30:05 +0530

    Delete assets directory

--- assets/js/login.js
+++
@@ -1,5 +0,0 @@
-const signupContainer = document.getElementById('signup-container');
-
-document.querySelector('.actions a').addEventListener('click', () => {
-    signupContainer.style.display = 'block';
-})
