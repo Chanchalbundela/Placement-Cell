a8affad8ea4124126d1b163fbecc8cc58afddb57
Chanchal <167846072+Chanchalbundela@users.noreply.github.com>
2024-10-26 20:31:08 +0530

    Delete routes directory

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-    res.redirect('/employee/login');
-})
-router.use('/employee', require('./employee'));
-
-module.exports = router;
